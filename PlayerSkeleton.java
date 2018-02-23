1acb493e9468eb2232acc6b80ca32aff9ddee6d1
FausticSun <FausticSun@gmail.com>
2018-02-23 16:29:37 +0800

    Implemented headless parameter

--- PlayerSkeleton.java
+++ PlayerSkeleton.java
@@ -1,27 +1,41 @@
+import java.util.Scanner;
 
 public class PlayerSkeleton {
+	static boolean HEADLESS = false;
 
-	//implement this function to have a working system
-	public int pickMove(State s, int[][] legalMoves) {
-		
-		return 0;
-	}
-	
 	public static void main(String[] args) {
+		for (String arg : args) {
+			switch(arg) {
+				case "headless":
+					HEADLESS = true; break;
+				default:
+					break;
+			}
+		}
+		new PlayerSkeleton();
+	}
+
+	public PlayerSkeleton() {
 		State s = new State();
-		new TFrame(s);
-		PlayerSkeleton p = new PlayerSkeleton();
+		if (!HEADLESS)
+			new TFrame(s);
 		while(!s.hasLost()) {
-			s.makeMove(p.pickMove(s,s.legalMoves()));
-			s.draw();
-			s.drawNext(0,0);
-			try {
-				Thread.sleep(300);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+			s.makeMove(pickMove(s,s.legalMoves()));
+			if (!HEADLESS) {
+				s.draw();
+				s.drawNext(0,0);
+				try {
+					Thread.sleep(300);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 		System.out.println("You have completed "+s.getRowsCleared()+" rows.");
 	}
-	
+
+	//implement this function to have a working system
+	public int pickMove(State s, int[][] legalMoves) {
+		return 0;
+	}
 }
